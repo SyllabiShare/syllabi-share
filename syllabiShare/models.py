eb0a94717b4d0f5741288136cad3510d41b242d5
Vernon Andrade <andradevern@gmail.com>
2020-05-19 13:56:14 -0400

    Add upload functionality

--- syllabiShare/models.py
+++ syllabiShare/models.py
@@ -5,5 +5,10 @@ class Submission(models.Model):
     user = models.TextField()
     prof = models.TextField()
     course = models.TextField()
+    school = models.TextField()
     upvotes = models.IntegerField
     add_date = models.DateTimeField('date added', default=timezone.now)
+
+class School(models.Model):
+    school = models.TextField(unique=True, blank=True)
+    domain = models.TextField(unique=True)
--- syllabiShare/views.py
+++ syllabiShare/views.py
@@ -1,38 +1,52 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
 from django.utils import timezone
-from .models import Submission
+from .models import Submission, School
+
+def authenticate(user):
+    if not user.is_authenticated:
+        return "error.html", {'loggedIn':False}
+    if user.email[-4:] != '.edu':
+        return "error.html", {'loggedIn':True}
+    return False,False
+
+def get_domain(email):
+    start = email.index('@')+1
+    end = email.index('.edu')
+    domain = email[start:end]
+    if len(School.objects.filter(domain=domain)) == 0:
+        school = School()
+        school.domain = domain
+        school.save()
+    return domain
 
 def index(request):
-    if not request.user.is_authenticated:
-        return render(request, "error.html", {'loggedIn':False})
-    if request.user.email[-4:] != '.edu':
-        logout(request)
-        return render(request, "error.html", {'loggedIn':True})
+    template,context = authenticate(request.user)
+    if template:
+        return render(request,template,context)
     return render(request, "index.html")
 
 def search(request):
-    if not request.user.is_authenticated:
-        return render(request, "error.html", {'loggedIn':False})
-    if request.user.email[-4:] != '.edu':
-        logout(request)
-        return render(request, "error.html", {'loggedIn':True})
+    template,context = authenticate(request.user)
+    if template:
+        return render(request,template,context)
     return render(request, "search.html")
 
 def upload(request):
+    template,context = authenticate(request.user)
+    if template:
+        return render(request,template,context)
+    success = False
     if request.method == 'POST':
         entry = Submission()
         entry.user = request.user.username
+        entry.school = get_domain(request.user.email)
         entry.prof = request.POST['prof']
         entry.course = request.POST['course']
         entry.upvotes = 1
         entry.add_date = timezone.now()
-    if not request.user.is_authenticated:
-        return render(request, "error.html", {'loggedIn':False})
-    if request.user.email[-4:] != '.edu':
-        logout(request)
-        return render(request, "error.html", {'loggedIn':True})
-    return render(request, "upload.html")
+        success = True
+    return render(request, "upload.html", {'success': success})
 
 def view404(request, exception=None):
     return redirect('/')
