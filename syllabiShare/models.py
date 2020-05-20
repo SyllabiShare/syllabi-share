6611c4117777f7973711353f6348ed2ae52d1e3f
Vernon Andrade <andradevern@gmail.com>
2020-05-20 02:28:43 -0400

    fix static file serving, move static back, clean requirements

--- syllabiShare/models.py
+++ syllabiShare/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.utils import timezone
 
 class Submission(models.Model):
     user = models.TextField()
     prof = models.TextField()
     course = models.TextField()
     school = models.TextField()
-    syllabus = models.FileField(blank=True)
+    syllabus = models.FileField(blank=True, upload_to='uploads/')
     upvotes = models.IntegerField(default = 1)
 
 class School(models.Model):
