1e0ff5e706c686f92ac8a0259080cf75549ea3b4
Vernon Andrade <andradevern@gmail.com>
2020-05-25 17:14:34 -0400

    close feedback as admin

--- syllabiShare/views.py
+++ syllabiShare/views.py
@@ -22,6 +22,8 @@ def admin(request):
                 User.objects.exclude(email__contains=".edu").delete()
             elif 'delete' in request.POST:
                 Submission.objects.get(pk=request.POST['pk']).delete()
+            elif 'close' in request.POST:
+                Suggestion.objects.get(pk=request.POST['pk']).delete()
             elif 'takedown' in request.POST and 'reason' in request.POST:
                 school = School.objects.get(pk=request.POST['pk'])
                 school.takedown = True
