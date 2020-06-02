d640a9749f06abda80a7055376fc85c2a7d60d4c
Winston Liu <Wliu1402@gmail.com>
2020-06-02 14:23:28 -0400

    Create school on UserProfile creation if necessary

--- syllabiShare/signals.py
+++ syllabiShare/signals.py
@@ -2,13 +2,16 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from syllabiShare.models import UserProfile
+from syllabiShare.models import UserProfile, School
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        start = instance.email.index('@') + 1
+        end = instance.email.index('.edu')
+        domain = instance.email[start:end]
+        UserProfile.objects.create(user=instance, school=School.objects.get_or_create(domain=domain))
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
