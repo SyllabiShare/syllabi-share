99329af1f780a07135fe91dc79c7b87a904c232d
Winston Liu <Wliu1402@gmail.com>
2020-06-02 23:53:49 -0400

    Revert "Signals suck; hook into the social auth pipeline instead"
    This reverts commit da542b424f7ea8d54182e997e6e05deca8377b66.

--- project/settings.py
+++ project/settings.py
@@ -127,19 +127,6 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {
     'prompt': 'select_account'
 }
 
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'syllabiShare.helpers.create_profile',  # create the UserProfile
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
-
 LOGIN_URL = '/auth/login/google-oauth2/'
 
 LOGIN_REDIRECT_URL = '/'
--- syllabiShare/helpers.py
+++
@@ -1,9 +0,0 @@
-from syllabiShare.models import UserProfile, School
-
-
-def create_profile(backend, user, response, *args, **kwargs):
-    start = user.email.index('@') + 1
-    end = user.email.index('.edu')
-    domain = user.email[start:end]
-    profile = UserProfile(user=user, school=School.objects.get_or_create(domain=domain)[0])
-    profile.save()
