ca7d842c23042b0a09de8826747cad4a273aba23
Winston Liu <Wliu1402@gmail.com>
2020-06-09 00:25:36 -0400

    Enforce .edu emails

--- syllabiShare/forms.py
+++ syllabiShare/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
 
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False)
     last_name = forms.CharField(max_length=30, required=False)
-    email = forms.EmailField(max_length=254, help_text='Enter a .edu email address')
+    email = forms.EmailField(help_text='Enter a .edu email address',
+                             validators=[RegexValidator(r'.*\.edu$', message='Email address must end in .edu')],
+                             widget=forms.EmailInput(attrs={'pattern': r'.*\.edu$',
+                                                            'title': 'Email address must end in .edu'}))
 
     class Meta:
         model = User
