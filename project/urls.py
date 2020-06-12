5caf0b73d131a4fda0002def0cae03ea16cb393f
Winston Liu <Wliu1402@gmail.com>
2020-06-12 16:09:12 -0400

    Move stuff around so we can use defaults

--- project/urls.py
+++ project/urls.py
@@ -26,22 +26,11 @@ urlpatterns = [
                                                 redirect_authenticated_user=True,
                                                 template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='landing.html'), name='logout'),
-    path('password-reset/', include([
-        path('', auth_views.PasswordResetView.as_view(
-            template_name='password-reset/password_reset_form.html',
-            email_template_name='password-reset/emails/password_reset.html',
-            form_class=auth_views.PasswordResetForm,
-            subject_template_name='password-reset/emails/password_reset_subject.txt',
-        ), name='password_reset'),
-        path('<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-            template_name='password-reset/password_reset_confirm.html',
-        ), name='password_reset_confirm'),
-        path('done/', auth_views.PasswordResetDoneView.as_view(
-            template_name='password-reset/password_reset_done.html',
-        ), name='password_reset_done'),
-        path('complete/', auth_views.PasswordResetCompleteView.as_view(
-            template_name='password-reset/password_reset_complete.html',
-        ), name='password_reset_complete'),
+    path('registration/', include([
+        path('', auth_views.PasswordResetView.as_view(), name='password_reset'),
+        path('done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+        path('<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+        path('complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     ])),
     path('', include('syllabiShare.urls')),
 ]
