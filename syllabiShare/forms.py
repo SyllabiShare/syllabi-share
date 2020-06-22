dc3dc89118e0b7834fb8e98a75c17cb84a5699a0
Winston Liu <Wliu1402@gmail.com>
2020-06-22 11:08:59 -0400

    We don't need personal info

--- syllabiShare/forms.py
+++ syllabiShare/forms.py
@@ -4,10 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core.validators import RegexValidator
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Row, Column
 
 
-class SimpleSignUpForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(help_text='Enter a .edu email address',
                              # RegexValidator matches on an arbitrary subset
                              validators=[RegexValidator(r'\.edu$', message='Email address must end in .edu')],
@@ -36,36 +35,6 @@ class SimpleSignUpForm(UserCreationForm):
         helper = FormHelper(self)
         helper.form_tag = False
         helper.use_custom_control = False
-
-        return helper
-
-
-class SignUpForm(SimpleSignUpForm):
-    first_name = forms.CharField(max_length=30, required=False)
-    last_name = forms.CharField(max_length=30, required=False)
-
-    class Meta:
-        model = User
-        fields = [
-            'email',
-            'first_name',
-            'last_name',
-            'password1',
-            'password2',
-        ]
-
-    @property
-    def helper(self):
-        helper = super().helper
-        helper.layout = Layout(
-            'email',
-            Row(
-                Column('first_name'),
-                Column('last_name'),
-            ),
-            'password1',
-            'password2',
-        )
 
         return helper
 
